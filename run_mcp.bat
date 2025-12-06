5852b4c778de3e853999917992c9b0ec2d4f867b
sunny <swarnendughosh05@gmail.com>
2025-12-06 23:51:11 +0530

    added mcp server with auto navigation in web

--- run_mcp.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d D:\coding\projects\tools\amazon-q-web_search
-wsl -e /mnt/d/coding/projects/tools/amazon-q-web_search/.venv/bin/python /mnt/d/coding/projects/tools/amazon-q-web_search/main.py
---
+++ run_mcp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /mnt/d/coding/projects/tools/amazon-q-web_search
+exec .venv/bin/python main.py
